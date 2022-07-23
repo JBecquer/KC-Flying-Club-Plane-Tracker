b766f98aa9b8f8c21295ecc3e7be3bd5dc572fed
JB <jjbecquer@gmail.com>
2022-07-22 21:17:28 -0500

    Add MySQL. Add dist_travelled logic.

--- main.py
+++ main.py
@@ -7,18 +7,87 @@ Author: Jordan Becquer
 
 import sys
 from math import radians, cos, sin, asin, sqrt
+import mysql.connector
 
 
 def flightaware_getter():
-    """API call to grab data from flight aware and save to file."""
+    """Web scraping to grab data from flight aware and save to file."""
+    pass
+
+
+def db_data_saver():
+    """Export the web scrapped data into MySQL"""
+
+    # Init connection to MySQL database
+    db = mysql.connector.connect(
+        host="localhost",
+        user="JBecquer",
+        passwd=input("Enter MySQL password: "),
+        database="N81673"
+    )
+
+    # TODO CLEAN-UP MYSQL SETUP
+    # initialize the MySQL cursor
+    mycursor = db.cursor()
+
+    # Create a database
+    # mycursor.execute("CREATE DATABASE [IF NOT EXIST] N81673")
+
+    # Create a table
+    # mycursor.execute("CREATE TABLE Flight (Date DATE, Time TIME, Latitude FLOAT, Longitude FLOAT, Knots TINYINT(3),
+    # Altitude MEDIUMINT(5))")
+
+    # Delete a table
+    # mycursor.execute("DROP TABLE Flight")
+
+    dummy_data = [["2022-7-22", "01:19:23", 42.1152, -92.9207, 88, 1000],
+                  ["2022-7-22", "01:19:39", 42.1193, -92.9279, 89, 1200],
+                  ["2022-7-22", "01:19:55", 42.1207, -92.9358, 89, 1400]
+                  ]
+
+    for step in dummy_data:
+        mycursor.execute("INSERT INTO Flight (Date, Time, Latitude, Longitude, Knots, Altitude) "
+                         "VALUES (%s,%s,%s,%s,%s,%s)",
+                         (step[0], step[1], step[2], step[3], step[4], step[5]))
+        db.commit()
+
+    mycursor.execute("SELECT * FROM Flight")
+
+    for x in mycursor:
+        print(x)
+
+
+def db_data_getter():
+    """Import the data from MySQL and convert into pandas """
+    db = mysql.connector.connect(
+        host="localhost",
+        user="JBecquer",
+        passwd=input("Enter MySQL password: "),
+        database="N81673"
+    )
+
+    mycursor = db.cursor()
     pass
 
 
 def calculate_stats():
     """ Calculate various stats related to the aircraft's history"""
+
+    # Init MySQL Database connection
+    db = mysql.connector.connect(
+        host="localhost",
+        user="JBecquer",
+        passwd=input("Enter MySQL password: "),
+        database="N81673"
+    )
+
     def dist_travelled():
-        """Calculate the total distance travelled by the aircraft using lat/long data."""
-        
+        """Calculate the total distance travelled by the aircraft using lat/long data.
+
+        :return: Total distance travelled in miles
+        :rtype: float(2)
+        """
+
         def lat_long_dist(lat1, lat2, lon1, lon2):
             """
             Calculate the distance between 2 sets of lat/long coordinates using the Haversine formula
@@ -38,7 +107,7 @@ def calculate_stats():
             # Haversine formula
             dlon = lon2 - lon1
             dlat = lat2 - lat1
-            a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+            a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
 
             c = 2 * asin(sqrt(a))
 
@@ -48,10 +117,23 @@ def calculate_stats():
             # calculate the result
             return c * r
 
+        # initialize the MySQL cursor
+        mycursor = db.cursor()
+
+        # grab the latitude and longitude data from MySQL
+        mycursor.execute("SELECT Latitude, Longitude FROM Flight")
+
+        latitude = []
+        longitude = []
+        for x in mycursor:
+            latitude.append(x[0])
+            longitude.append(x[1])
+
         total_dist = 0
-        
-        #for i, coord in enumerate(coordinates[:-1:]):
-            # total_dist += lat_long_dist(coord[i][0], coord[i][1], coord[i+1][0], coord[i+1][1]
+        # TODO CREATE AN EXCEPTION FOR WHEN LEN(LAT) != LEN(LONG)
+        for x in range(len(latitude[:-1:])):
+            total_dist += lat_long_dist(latitude[x], latitude[x + 1], longitude[x], longitude[x + 1])
+        print(f" The total distance travelled was {round(total_dist, 2)} Miles")
         return total_dist
 
     def time_aloft():
@@ -61,6 +143,11 @@ def calculate_stats():
     def airports_visited():
         """Determine the airports visited"""
         pass
+
+    # calculate_stats function calls
+    dist_travelled()
+    time_aloft()
+    airports_visited()
 
 
 def local_area_map():
@@ -79,4 +166,6 @@ def main():
 
 
 if __name__ == "__main__":
+    # db_data_saver()
+    calculate_stats()
     sys.exit(main())
